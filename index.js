16c2998598aa3679941a64586e6f0a9ea2ce6749
Maltazard <markus.yarleque@gmail.com>
2021-11-18 04:19:33 -0500

    mas

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 
 const { Client, Intents } = require('discord.js');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
 
 // const { MessageSelectMenu, MessageEmbed } = require('discord.js')
 
