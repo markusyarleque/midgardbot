b04837cce87e5132e1d5fdfc85cfc71d88790681
Maltazard <markus.yarleque@gmail.com>
2021-12-22 19:25:16 -0500

    Msa

--- index.js
+++ index.js
@@ -1385,8 +1385,8 @@ client.on('messageCreate', async message => {
       } else {
 
         let cooldown = ((buscarUsuario.daily - Date.now())/1000)
-        let h = (cooldown/3600).toFixed()
-        let m = ((cooldown % 3600)/60).toFixed()
+        let h = ((cooldown / 3600)-1).toFixed()
+        let m = ((((cooldown % 3600)-1)/60)-1).toFixed()
         let mensaje
 
         // condition ? val1 : val2 
