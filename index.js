60a84c1d6c1b1eeb5e59208167ab2724a7648f48
Maltazard <markus.yarleque@gmail.com>
2021-12-27 04:29:18 -0500

    myou

--- index.js
+++ index.js
@@ -89,6 +89,10 @@ const MENSAJE_AYUDA_CARTA = new Discord.MessageEmbed()
 
 const prefix = process.env.PREFIX;
 
+const ytdl = require('ytdl-core');
+const search = require('youtube-search');
+const queue = new Map();
+
 client.on('ready', () => {
   
   client.user.setPresence({
@@ -705,6 +709,8 @@ client.on('messageCreate', async message => {
 
     if(!message.content.startsWith(process.env.PREFIX)) return;
     
+    const serverQueue = queue.get(message.guild.id);
+
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
@@ -1509,7 +1515,7 @@ client.on('messageCreate', async message => {
 
     }
 
-    // COMANDOS DE PROGRAMADO
+    // COMANDOS DE PROGRAMADOR
 
     if(command === 'malta'){
 
@@ -1541,6 +1547,146 @@ client.on('messageCreate', async message => {
         }
   
       }
+
+
+    // COMANDOS DE MÚSICA
+
+    // <-- CODIGO CMD PLAY (REPRODUCIR): -->
+
+    if(command === 'play') {
+
+      const voiceChannel = message.member.voice.channel;
+  
+      //verificamos que el usuario solicitante este conectado en un canal de voz.
+      if (!voiceChannel) return message.channel.send({embeds: [
+        new Discord.MessageEmbed()
+        .setAuthor(message.author.tag, message.author.displayAvatarURL())
+        .setColor('RED')
+        .setDescription(`<a:Verify2:880315278347616329> | ¡Necesitas unirte a un canal de voz para reproducir música!`)
+      ]});
+  
+      const permissions = voiceChannel.permissionsFor(message.client.user);
+  
+      //verificamos que el bot tenga permisos de conectar y de hablar en el canal de voz.
+      if (!permissions.has(Permissions.FLAGS.CONNECT) || !permissions.has(Permissions.FLAGS.SPEAK)) {
+
+        return message.channel.send({embeds: [
+          new Discord.MessageEmbed()
+          .setAuthor(message.author.tag, message.author.displayAvatarURL())
+          .setColor('RED')
+          .setDescription(`<a:Verify2:880315278347616329> | ¡Necesito permisos para unirme y hablar en el canal de voz!`)
+        ]});
+      
+      }
+    
+      // <-- Capturamos la información de la música a reproducir -->
+  
+      var opts = {
+        maxResults: 1, //Maximo de resultados a encontrar
+        key: 'AIzaSyAVNU3oD7NRVzc6CfbV14fBw__bP5lRQM0', //Necesitas una CLAVE de la API de youtube. 
+        type: "video" // Que tipo de resultado a obtener.
+      };
+
+      const songArg = await search(args.join(' '), opts);
+      const songURL = songArg.results[0].link;
+      const songInfo = await ytdl.getInfo(songURL);
+
+      const song = {
+        title: songInfo.title,
+        url: songInfo.video_url,
+        author: message.author.tag
+      };
+
+      // <-- Verificamos la lista de canciones de un servidor -->
+      
+      if (!serverQueue) {
+
+        // Si NO hay una lista de música.
+        // <-- Creamos nuestra cola de música a reproducir  -->
+
+        // Creamos el conjunto de datos para nuestra cola de música
+
+        const queueObject = {
+          textChannel: message.channel, //guardamos el canal de texto
+          voiceChannel: voiceChannel, // guardamos el canal de voz
+          connection: null, // un objeto para la conexión 
+          songs: [], // creamos la lista de canciones
+          volume: 5, // volumen al iniciar la cola
+          playing: true, // un objeto para validar la cola de música en reproducción.
+        };
+
+        // <-- Establecer la cola de música  -->
+
+        // Creando el conjunto de datos para nuestra cola de música
+        queue.set(message.guild.id, queueObject);
+
+        // Agregamos las canciones al conjunto de datos
+        queueObject.songs.push(song);
+
+        // <-- Conectar al canal de voz  -->
+      
+        try {
+
+          // Aquí unimos el bot al canal de voz y guardar nuestra conexión en nuestro objeto.
+          var connection = await voiceChannel.join();
+          queueObject.connection = connection;
+         
+          message.channel.send({embeds: [
+          new Discord.MessageEmbed()
+          .setAuthor(message.author.tag, message.author.displayAvatarURL())
+          .setColor('GREEN')
+          .setDescription(`<a:Verify1:880315279391985744> | Reproduciendo ahora: **${song.title}**`)
+          .setTimestamp()
+          ]});
+         
+          // Llamar a la función de reproducción para comenzar una canción.
+          play(message.guild, queueObject.songs[0]);
+         
+        } catch (err) {
+         
+          // Imprimir el mensaje de error si el bot no puede unirse al chat de voz
+          console.log(err);
+          queue.delete(message.guild.id);
+          return message.channel.send({embeds: [
+            new Discord.MessageEmbed()
+            .setAuthor(message.author.tag, message.author.displayAvatarURL())
+            .setColor('RED')
+            .setDescription(`<a:Verify2:880315278347616329> | ${err}`)
+          ]});
+         
+        }
+
+      }else {
+
+        // Si HAY una lista de música reproduciendo.
+      
+        serverQueue.songs.push(song);
+        console.log(serverQueue.songs);
+
+        return message.channel.send({embeds: [
+          new Discord.MessageEmbed()
+          .setAuthor(message.author.tag, message.author.displayAvatarURL())
+          .setColor('GREEN')
+          .setDescription(`<a:Verify1:880315279391985744> | **${song.title}** ha sido añadido a la cola!, __por: ${message.author.tag}__`)
+          .setTimestamp()
+        ]});
+      
+      }
+  
+    }
+
+    // <-- CODIGO CMD SKIP (SALTAR): -->
+
+    // <-- CODIGO CMD STOP (DETENER): -->
+
+    // <-- CODIGO CMD VOLUMEN (VOLUMEN): -->
+
+    // <-- CODIGO CMD PAUSAR (PAUSE): -->
+
+    // <-- CODIGO CMD RESUME (RESUME): -->
+
+    // <-- CODIGO CMD QUEUE (QUEUE): -->
+
 
     // COMANDOS DE INFORMACIÓN
 
@@ -10370,3 +10516,34 @@ client.on('warn', (e) => console.warn(e));
 client.on('debug', (e) => console.info(e));
 
 client.login(process.env.TOKEN); 
+
+// <-- FUNCION PLAY (REPRODUCIR): -->
+
+function play(guild, song) {
+
+  const serverQueue = queue.get(guild.id);
+
+  // verificamos que hay musica en nuestro objeto de lista
+  if (!song) {
+   serverQueue.voiceChannel.leave(); // si no hay mas música en la cola, desconectamos nuestro bot
+   queue.delete(guild.id);
+   return;
+  }
+ 
+  // <-- Reproducción usando play()  -->
+ 
+  const dispatcher = serverQueue.connection.play(ytdl(song.url))
+    .on('finish', () => {
+    // Elimina la canción terminada de la cola.
+    serverQueue.songs.shift();
+
+    // Llama a la función de reproducción nuevamente con la siguiente canción
+    play(guild, serverQueue.songs[0]);
+    })
+    .on('error', error => {
+    console.error(error);
+    });
+
+    // Configuramos el volumen de la reproducción de la canción
+    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+ }
