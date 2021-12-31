ff013c41739d6eeae7e6a6c892e82f656d4d677f
Maltazard <markus.yarleque@gmail.com>
2021-12-30 22:12:47 -0500

    md

--- index.js
+++ index.js
@@ -118,7 +118,31 @@ client.on('guildMemberAdd', async member => {
   console.log(`Nuevo usuario:  ${member.user.username} se ha unido a ${guild.name}.`);
 
   const channel = guild.channels.cache.find(ch => ch.id === '779566489429737472');
-  
+
+  var welcome = [
+    'https://media.discordapp.net/attachments/853500788848853002/873245600936788048/1.gif',
+    'https://media.discordapp.net/attachments/853500788848853002/873245604090892348/2.gif',
+    'https://media.discordapp.net/attachments/853500788848853002/873245605294645308/3.gif',
+    'https://media.discordapp.net/attachments/853500788848853002/873245608775917688/4.gif',
+    'https://media.discordapp.net/attachments/853500788848853002/873245613217689650/5.gif',
+    'https://media.discordapp.net/attachments/853500788848853002/873245612554993704/6.gif',
+    'https://media.discordapp.net/attachments/853500788848853002/873245617277796394/7.gif',
+    'https://media.discordapp.net/attachments/853500788848853002/873245617672056902/8.gif',
+    'https://media.discordapp.net/attachments/853500788848853002/873245622017359962/9.gif',
+    'https://media.discordapp.net/attachments/853500788848853002/873245621820215326/10.gif'
+  ] 
+
+  let ramdonwelcome = welcome[Math.floor(Math.random()*welcome.length)]
+   
+  const embed = new Discord.MessageEmbed()
+  .setThumbnail(`${member.displayAvatarURL({ dynamic: true }).replace('webp','png')}`)
+  .setTitle(`Bienvenid@ Terrícola! **${member.user.username}** <:cosita:925928055745155113> , a este nuestro **Universo**. <a:pepedance:880928616416968745>`)
+  .setDescription(`<:shylove:925923567776964619> Gracias por unirte <a:exclama2:880930071731392512> <a:sc_ositobailin:880930467774365707> Espero que lo pases genial en este server libre de toxicidad <a:abdul_dance:880930576683630662>, con muchos eventos programados, premios y más sorpresas!!! <a:Sara:880304101215334401> Recuerda pasar por <#777623227321155614> y <#880268322921414657> Y si tienes alguna queja, duda o sugerencia, pasa por <#880402803825188874>. Cualquier incoveniente aquí estará todo el equipo de Staff a su disposición. <a:dc_party1:881033439367815239>`)
+  .setImage(ramdonwelcome)
+  .setColor('RANDOM')
+  .setTimestamp(new Date())
+  .setFooter(guild.name, guild.iconURL({ dynamic: true }));
+
   if (!channel) return;
 
   /*if (newUsers.size > 1000000) {
@@ -129,8 +153,8 @@ client.on('guildMemberAdd', async member => {
 
   }*/
 
-  channel.send('¡Bienvenid@ Terrícola!' + `${member.user}` + ', a este nuestro **Universo**. <:shylove:925923567776964619>. Espero que te lo pases genial en este server libre de toxicidad, con muchos eventos programados, premios y más sorpresas!!! <a:Sara:880304101215334401> Recuerda pasar por <#777623227321155614> y <#880268322921414657> Y si tienes alguna queja, duda o sugerencia, pasa por <#880402803825188874>. Cualquier incoveniente aquí estará todo el equipo de Staff a su disposición. <a:dc_party1:881033439367815239>');
-  
+  //channel.send('¡Bienvenid@ Terrícola!' + `${member.user}` + ', a este nuestro **Universo**. <:shylove:925923567776964619>. Espero que te lo pases genial en este server libre de toxicidad, con muchos eventos programados, premios y más sorpresas!!! <a:Sara:880304101215334401> Recuerda pasar por <#777623227321155614> y <#880268322921414657> Y si tienes alguna queja, duda o sugerencia, pasa por <#880402803825188874>. Cualquier incoveniente aquí estará todo el equipo de Staff a su disposición. <a:dc_party1:881033439367815239>');
+  channel.send({embeds:[embed]})
     
 });
 
