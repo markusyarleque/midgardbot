22d389f693879ba9ef8bf018b6f6ea3609be2c6c
Maltazard <markus.yarleque@gmail.com>
2022-01-11 01:20:28 -0500

    MSS

--- index.js
+++ index.js
@@ -3098,8 +3098,10 @@ client.on('messageCreate', async message => {
 
       }
 
-      let userm = message.mentions.users.first() || message.guild.members.resolve(args[0]) || message.guild.members.cache.find(m => m.user.username.toLowerCase() === args[0]) || await client.users.fetch(args[0])
+      let userm = message.mentions.users.first() || message.guild.members.resolve(args[0]) || message.guild.members.cache.find(m => m.user.username.toLowerCase() === args[0])
 
+      await client.users.fetch(userm)
+      
       if(!userm){
             
         var user = message.author;
