00bb829aff3cf15515de172bd8c95ea56cd755e5
Maltazard <markus.yarleque@gmail.com>
2022-01-13 03:56:01 -0500

    MSA

--- index.js
+++ index.js
@@ -1677,14 +1677,14 @@ client.on('messageCreate', async message => {
             
             m.edit({
               embeds: [shop1],
-              components: [b1]
+              components: [new MessageActionRow().addComponents([bS1,bS2])]
             });
   
           }else if (int.customId === "p2") {
             
             m.edit({
               embeds: [shop2],
-              components: [b1]
+              components: [new MessageActionRow().addComponents([bS1,bS2])]
             });
   
           }
