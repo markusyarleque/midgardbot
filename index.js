78601ed9abde9dd080e633df92229335e7d8ba99
Maltazard <markus.yarleque@gmail.com>
2022-01-14 05:24:00 -0500

    MMMM

--- index.js
+++ index.js
@@ -3416,7 +3416,7 @@ client.on('messageCreate', async message => {
           const snipe = +args[0] - 1 || 0
           const targer = snipes[snipe]
 
-          if(!targer)message.reply({ allowedMentions: { repliedUser: false}, content: `${emoji[2]} **Solamente hay ${snipes.length} snipes**`})
+          if(!targer)message.reply({ allowedMentions: { repliedUser: false}, content: `**Solamente hay ${snipes.length} snipes**`})
 
           const {msg, time, image, canal} = targer
     
