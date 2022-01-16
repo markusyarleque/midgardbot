129603fff8c9cf24b41135fb59c01c302b530e0c
Maltazard <markus.yarleque@gmail.com>
2022-01-16 14:46:28 -0500

    mcafe

--- index.js
+++ index.js
@@ -5209,13 +5209,6 @@ client.on('messageCreate', async message => {
     
         let img = message.guild.members.resolve(message.mentions.users.first() || client.users.cache.get(args[0]));
         let ramdoncafe = cafe[Math.floor(Math.random()*cafe.length)]
- 
-        if (img.bot) return message.channel.send({embeds: [
-          new Discord.MessageEmbed()
-          .setAuthor(message.author.tag, message.author.displayAvatarURL())
-          .setColor('RED')
-          .setDescription(`<a:Verify2:931463492677017650> | Mucho café nos pone nerviosos! <:nogarsias:932172183453712415>`)
-        ]})
 
         if (!img || img.id === message.author.id) {
     
@@ -5229,6 +5222,15 @@ client.on('messageCreate', async message => {
             .setFooter(`${message.guild.name}`,'https://media.discordapp.net/attachments/880312288593195028/904603928375726120/Midgard_GIF_AVATAR.gif');
             message.channel.send({ embeds: [embed] });
     
+        } else  if (img.user.bot) {
+
+          return message.channel.send({embeds: [
+          new Discord.MessageEmbed()
+          .setAuthor(message.author.tag, message.author.displayAvatarURL())
+          .setColor('RED')
+          .setDescription(`<a:Verify2:931463492677017650> | Mucho café nos pone nerviosos! <:nogarsias:932172183453712415>`)
+          ]})
+          
         } else {
     
             const embed = new Discord.MessageEmbed()
