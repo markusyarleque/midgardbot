64e0f84629040eb752b2674338152c29dda84f03
Maltazard <markus.yarleque@gmail.com>
2022-01-22 08:45:53 -0500

    midtbot

--- eventos/messageCreate.js
+++ eventos/messageCreate.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
 
+const { Client, Intents } = require('discord.js');
+
 const client = new Client({ allowedMentions: { parse: ['users'], repliedUser: true }, intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.GUILD_PRESENCES] });
 
 const { MessageActionRow, MessageButton } = require('discord.js');
