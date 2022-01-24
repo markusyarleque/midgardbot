79f4d01a71da343ea6f537b1b9e5d9598ae13fe7
Maltazard <markus.yarleque@gmail.com>
2022-01-23 23:21:59 -0500

    mb

--- commands/reacción/sape.js
+++ commands/reacción/sape.js
@@ -1,3 +1,8 @@
+let i = 'https://c.tenor.com/FLR3dFSlH1sAAAAC/bully-tierno.gif'
+let f = 'No hay frase agregada'
+let color = '#607D8B'
+let marry = 'Soltero(a)'
+
 module.exports =  {
     
     name: 'sape',
