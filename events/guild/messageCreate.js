3ff702a8b213aa49ed5f0d8eea5c9b8f7222397d
Maltazard <markus.yarleque@gmail.com>
2022-01-30 01:06:57 -0500

    mb

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -385,12 +385,12 @@ module.exports = async (client, Discord, message) => {
     
     }
 
-    var mencionbot = message.content.startsWith('<@'+client.user.id+'>') || message.content.startsWith('midgard') || message.content.startsWith('mid') || message.content.startsWith('mb')
+    var mencionbot = message.content === '<@'+client.user.id+'>' || message.content.startsWith('midgard') || message.content.startsWith('mid') || message.content.startsWith('mb')
     console.log(mencionbot+' mencion: '+client.user.id)
 
     if(!message.content.startsWith(prefix) && !mencionbot) return; 
   
-    const args = message.content.slice(prefix.length).trim().split(/ +/g)
+    const args = message.content.slice(prefix.length || mencionbot.length).trim().split(/ +/g)
     const command = args.shift().toLowerCase()
 
     if(bl.tiene(message.author.id)) {
