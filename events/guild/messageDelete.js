80586fb360d64037aac7dd8581b5a0dcead607ad
Maltazard <markus.yarleque@gmail.com>
2022-01-30 05:43:48 -0500

    mb

--- events/guild/messageDelete.js
+++ events/guild/messageDelete.js
@@ -1,6 +1,6 @@
-module.exports = async (client, message, Discord) => {
+/*module.exports = async (client, message, Discord) => {
 
-  let snipes = [] 
+  let snipes = client.snipes.get(message.channel.id) || [] 
 
   if(snipes.length > 10) snipes = snipes.slice(0, 10)
   
@@ -31,4 +31,4 @@ module.exports = async (client, message, Discord) => {
     
   channel.send({ embeds: [embed] });
     
-}
+}*/
