746d5b8b591642ae2b8d5e5deb2b6cf61b50e76c
Maltazard <markus.yarleque@gmail.com>
2022-02-10 21:38:12 -0500

    mb

---
+++ commands/diversión/morse.js
@@ -0,0 +1,70 @@
+module.exports =  {
+    
+    name: 'morse',
+    aliases: [],
+    description: '🌐 Convierte un texto en código Morse o viceversa.',
+  
+    async execute(client, message, args, Discord) {
+
+        let alpha = " ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".split("")
+		let morse = "/,.-,-...,-.-.,-..,.,..-.,--.,....,..,.---,-.-,.-..,--,-.,---,.--.,--.-,.-.,...,-,..-,...-,.--,-..-,-.--,--..,.----,..---,...--,....-,.....,-....,--...,---..,----.,-----".split(",")
+		let text = args.slice(1).join(" ").toUpperCase()
+        let title
+
+        while (text.includes("Ä") || text.includes("Ö") || text.includes("Ü")) {
+            
+            text = text.replace("Ä", "AE").replace("Ö", "OE").replace("Ü", "UE")
+            
+        }
+
+        if (text.startsWith(".") || text.startsWith("-")) {
+
+			//Separar el texto morse en array
+			text = text.split(" ");
+			//Longitud
+			let length = text.length;
+			//Convertir a texto normal usando un loop
+			for (let i = 0; i < length; i++) {
+
+				text[i] = alpha[morse.indexOf(text[i])];
+
+			}
+
+			//Volver a unir
+			text = text.join("");
+            title = '💫 Morse a Texto'
+
+		} else {
+
+			//Lo mismo. Separar
+			text = text.split("");
+			let length = text.length;
+			//Convertir a morse
+
+			for (let i = 0; i < length; i++) {
+
+				text[i] = morse[alpha.indexOf(text[i])];
+
+			}
+
+			text = text.join(" ");
+            title = '💫 Texto a Morse'
+
+		}
+        
+        const embed = new Discord.MessageEmbed()
+        .setThumbnail('https://c.tenor.com/Q17dDRR4Dd8AAAAC/morse-code.gif')
+        .setAuthor(`Midgard's Fun`,message.guild.iconURL({ dynamic: true }))
+        .setTitle(title)
+        .setDescription('Autor: '+message.author.username+'#'+message.author.discriminator)
+        .addField('Texto:', "```" + text + "```", true)
+        .addField('Morse:', "```" + text + "```", true)
+        .setColor('RANDOM')
+        .setTimestamp(new Date())
+        .setFooter(`${message.guild.name}`,'https://media.discordapp.net/attachments/880312288593195028/904603928375726120/Midgard_GIF_AVATAR.gif');
+             
+		message.channel.send({ embeds: [embed]});
+        
+    }
+
+}
