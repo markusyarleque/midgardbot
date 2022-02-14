34b758d5de1942f6c38aaea9a51fa53851afd17a
Maltazard <markus.yarleque@gmail.com>
2022-02-14 05:44:40 -0500

    mb

--- events/guild/guildCreate.js
+++ events/guild/guildCreate.js
@@ -6,12 +6,12 @@ module.exports = async (client, Discord, guild) => {
 
     const embed = new Discord.MessageEmbed()
     .setAuthor('MaltaBot',client.user.avatarURL())
-    .setThumbnail(guild.iconURL({ dynamic: true }) ? guild.iconURL({ dynamic: true }) : 'https://i.pinimg.com/originals/04/41/ea/0441ea5619b979bde781aa040943c208.gif')
+    .setThumbnail(guild.iconURL() ? guild.iconURL({ dynamic: true }) : 'https://i.pinimg.com/originals/04/41/ea/0441ea5619b979bde781aa040943c208.gif')
     .setTitle(`¡Server nuevo! <:abby:931432327354155038>`)
     .setDescription('<:shylove:931432905421520927> Me he unido al servidor: \n\n> Nombre: '+guild.name+'\n\n> Id: '+guild.id+'\n\n> Fecha: Hoy\n\n> Invitación: ')
     .setColor('RANDOM')
     .setTimestamp(new Date())
-    .setFooter(guild.name, guild.bannerURL({ dynamic: true, size: 4096 }).replace('webp','png') ? guild.bannerURL({ dynamic: true, size: 4096 }).replace('webp','png') : 'https://www.cosas-que-pasan.com/wp-content/uploads/portada-facebook-en-construccion.jpg');
+    .setFooter(guild.name, guild.bannerURL() ? guild.bannerURL({ dynamic: true, size: 4096 }).replace('webp','png') : 'https://www.cosas-que-pasan.com/wp-content/uploads/portada-facebook-en-construccion.jpg');
 
     try {
 
