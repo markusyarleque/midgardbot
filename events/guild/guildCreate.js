6919c7f9728544aaef7884c1b0e6f3a3befb0681
Maltazard <markus.yarleque@gmail.com>
2022-02-15 01:59:42 -0500

    mb

--- events/guild/guildCreate.js
+++ events/guild/guildCreate.js
@@ -3,12 +3,14 @@ module.exports = async (client, Discord, guild) => {
     let owner = client.users.cache.get('753435606410985573')
     let sv = client.guilds.cache.get('777620055344545842')
     let channel = sv.channels.cache.get('874943743185285150')
-
+    let invite = await guild.createInvite({ unique: true })
+        
+    console.log('Link : '+invite)
     const embed = new Discord.MessageEmbed()
     .setAuthor('MaltaBot',client.user.avatarURL())
     .setThumbnail(guild.iconURL() ? guild.iconURL({ dynamic: true }) : 'https://i.pinimg.com/originals/04/41/ea/0441ea5619b979bde781aa040943c208.gif')
     .setTitle(`¡Server nuevo! <:abby:931432327354155038>`)
-    .setDescription('<:shylove:931432905421520927> Me he unido al servidor: \n\n> Nombre: '+guild.name+'\n\n> Id: '+guild.id+'\n\n> Fecha: Hoy\n\n> Invitación: ')
+    .setDescription('<:shylove:931432905421520927> Me he unido al servidor: \n\n> Nombre: '+guild.name+'\n> \n> Id: '+guild.id+'\n> \n> Fecha: Hoy\n> \n> Invitación: ')
     .setColor('RANDOM')
     .setTimestamp(new Date())
     .setFooter(guild.name, guild.bannerURL() ? guild.bannerURL({ dynamic: true, size: 4096 }).replace('webp','png') : 'https://www.cosas-que-pasan.com/wp-content/uploads/portada-facebook-en-construccion.jpg');
