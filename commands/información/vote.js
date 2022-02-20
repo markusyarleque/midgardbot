a009e835f59906432d91ee2ed070ed7ecdbffe39
Maltazard <markus.yarleque@gmail.com>
2022-02-20 04:39:37 -0500

    mb

--- commands/información/vote.js
+++ commands/información/vote.js
@@ -8,7 +8,7 @@ module.exports =  {
   
     async execute(client, message, args, Discord) {
 
-        const votado = await client.dbl.hasVoted(message.author.id)
+        //const votado = await client.dbl.hasVoted(message.author.id)
 
         const embed = new Discord.MessageEmbed()
         .setAuthor(`Midgard's Staff`,message.guild.iconURL({ dynamic: true }))
@@ -32,20 +32,22 @@ module.exports =  {
 
         );
 
-        if(!votado) return message.reply({ allowedMentions: { repliedUser: false}, embeds : [embed], components: [row] });
-
-        const embed2 = new Discord.MessageEmbed()
-        .setAuthor(`Midgard's Staff`,message.guild.iconURL({ dynamic: true }))
-        .setThumbnail('https://logos-marcas.com/wp-content/uploads/2020/12/Discord-Logo.png')
-        .setTitle('Ya has Votado por Nuestro Servidor <a:gatoasomar:930399873113677834>')
-        .setDescription(`Estamos eternamente agradecidos de que votes por Midgard! <:abby:931432327354155038>`)
-        .addField('Puedes volver a votar en:','time',false)
-        .addField('Recompensas por tu voto <a:corazon:930399275643453500>','Recibirás un Rol muy especial que te diferenciará de los demás:\n\n <a:dc_zFlecha3:880315279903703060> <@&886772939549184052> \n\nAdemás, este rol te dará una cierta cantidad de coins de nuestro casino cada cierto tiempo... \n\nDescúbrelo!!! <:tierno:931433334960160799>',true)
-        .setColor('RANDOM')
-        .setTimestamp(new Date())
-        .setFooter(`${message.guild.name}• Muchas gracias por tu voto!`,'https://media.discordapp.net/attachments/880312288593195028/904603928375726120/Midgard_GIF_AVATAR.gif');
+        //if(!votado) 
         
-        message.reply({ embeds : [embed2] });
+        message.reply({ allowedMentions: { repliedUser: false}, embeds : [embed], components: [row] });
+
+        // const embed2 = new Discord.MessageEmbed()
+        // .setAuthor(`Midgard's Staff`,message.guild.iconURL({ dynamic: true }))
+        // .setThumbnail('https://logos-marcas.com/wp-content/uploads/2020/12/Discord-Logo.png')
+        // .setTitle('Ya has Votado por Nuestro Servidor <a:gatoasomar:930399873113677834>')
+        // .setDescription(`Estamos eternamente agradecidos de que votes por Midgard! <:abby:931432327354155038>`)
+        // .addField('Puedes volver a votar en:','time',false)
+        // .addField('Recompensas por tu voto <a:corazon:930399275643453500>','Recibirás un Rol muy especial que te diferenciará de los demás:\n\n <a:dc_zFlecha3:880315279903703060> <@&886772939549184052> \n\nAdemás, este rol te dará una cierta cantidad de coins de nuestro casino cada cierto tiempo... \n\nDescúbrelo!!! <:tierno:931433334960160799>',true)
+        // .setColor('RANDOM')
+        // .setTimestamp(new Date())
+        // .setFooter(`${message.guild.name}• Muchas gracias por tu voto!`,'https://media.discordapp.net/attachments/880312288593195028/904603928375726120/Midgard_GIF_AVATAR.gif');
+        
+        // message.reply({ embeds : [embed2] });
         
     }
 
