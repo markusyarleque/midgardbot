f764864605701a8bd3476266c18e6839e0279c31
Maltazard <markus.yarleque@gmail.com>
2022-02-21 02:22:29 -0500

    mb

--- events/guild/interactionCreate.js
+++ events/guild/interactionCreate.js
@@ -2,27 +2,27 @@ module.exports = (client, Discord, interaction) => {
 
     //? BUTTONS
     
-    if(interaction.isButton()){
+    // if(interaction.isButton()){
 
-        const member = interaction.member
+    //     const member = interaction.member
 
-        if(interaction.customId === 'acprueba'){
+    //     if(interaction.customId === 'acprueba'){
             
-            interaction.deferReply({ ephemeral: true})
-            interaction.followUp({ content: 'Hola'})
+    //         interacation.deferReply({ ephemeral: true})
+    //         interaction.followUp({ content: 'Hola'})
 
-            return console.log('Prueba aceptada por el usuario: '+member)
+    //         return console.log('Prueba aceptada por el usuario: '+member)
             
-        }
-        if(interaction.customId === 'denyprueba'){
+    //     }
+    //     if(interaction.customId === 'denyprueba'){
 
-            interaction.deferReply({ ephemeral: true})
-            interaction.followUp({ content: 'Hola'})
+    //         interaction.deferReply({ ephemeral: true})
+    //         interaction.followUp({ content: 'Hola'})
 
-            return console.log('Prueba denegada')
-        }
+    //         return console.log('Prueba denegada')
+    //     }
 
-    }
+    // }
 
     //? BUTTONS
 
