6f15b8057b8a5f9f89ddd2d4a8a80afb4da709ad
Maltazard <markus.yarleque@gmail.com>
2022-02-21 01:30:02 -0500

    mb

---
+++ commands/diversión/footballmatch.js
@@ -0,0 +1,36 @@
+const { FootballMatch } = require('leaf-utils');
+
+module.exports =  {
+    
+    name: 'footballmatch',
+    aliases: ['football','futbol'],
+    description: '⚽ Juega Football Match',
+  
+    async execute(client, message, args, Discord) {
+
+        await FootballMatch({
+            message: message,
+            slash_command: false,
+            embed: {
+                title: 'Football Match',
+                color: 'RANDOM',
+            },
+            buttons: {
+                left: 'Izquierda',
+                middle: 'Al Medio',
+                right: 'Derecha',
+            },
+            emojis: {
+                goalkeeper: '🧍‍♂️',
+                goal: '🥅',
+                soccer: '⚽',
+            },
+            winMessage: 'GG, <@{{winner}}> anotó en **{{time}} segundos**.',
+            loseMessage: '<@{{player}}> Perdiste 😢',
+            ongoingMessage: 'Hay un juego ejecutándose en <#{{channel}}>. No puedes jugar ahora!',
+            authorOnly: 'Solo <@{{author}}> puede usar estos botones!',
+        })
+
+    }
+
+}
