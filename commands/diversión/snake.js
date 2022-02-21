60bf06675d80b3c8b55169c69e8c7aee14d7a63b
Maltazard <markus.yarleque@gmail.com>
2022-02-21 01:10:30 -0500

    mb

---
+++ commands/diversión/snake.js
@@ -0,0 +1,38 @@
+const { Snake } = require('leaf-utils');
+
+module.exports =  {
+    
+    name: 'snake',
+    aliases: ['gusanito'],
+    description: '🐛 Clásico juego del Snake',
+  
+    async execute(client, message, args, Discord) {
+
+        new Snake({
+            message: message,
+            slash_command: false,
+            snake: {
+                head: '🟢',
+                body: '🟩',
+                tail: '🟢',
+                over: '💀'
+            },
+            emojis: {
+                board: '⬛',
+                food: '🍎',
+                up: '⬆️',
+                right: '➡️',
+                down: '⬇️',
+                left: '⬅️',
+            },
+            foods: ['🍎', '🍇', '🍊', '🍕', '🥕', '🥞'],
+            stopButton: {
+                stopLabel: 'Stop',
+                stopStyle: 'DANGER',
+            },
+            authorOnly: 'Solo {{author}} puede usar estos botones.',
+        }).startGame();
+
+    }
+
+}
