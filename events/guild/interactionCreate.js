ac7af593ab0e2c0e6e7a00dc8ebe5de9f4f1de90
Maltazard <markus.yarleque@gmail.com>
2022-02-24 01:56:47 -0500

    mb

---
+++ events/guild/interactionCreate.js
@@ -0,0 +1,51 @@
+module.exports = (client, Discord, interaction) => {
+
+    //? BUTTONS
+  
+    /*if(interaction.isButton()){
+
+        const member = interaction.member
+
+        if(interaction.customId === 'acprueba'){
+          
+            interacation.deferReply({ ephemeral: true})
+            interaction.followUp({ content: 'Hola'})
+
+            return console.log('Prueba aceptada por el usuario: '+member)
+          
+        }
+        
+        if(interaction.customId === 'denyprueba'){
+
+            interaction.deferReply({ ephemeral: true})
+            interaction.followUp({ content: 'Hola'})
+
+            return console.log('Prueba denegada')
+
+        }
+
+    }*/
+
+    //? BUTTONS
+
+    //* SLASH COMMANDS
+    
+    if(interaction.isCommand()){
+
+        const command = client.slash.get(interaction.commandName)
+
+        try {
+            
+            command.run(client, interaction)
+
+        } catch (error) {
+            
+            console.log('Error en la IC: '+command + ' - '+error)
+
+        }
+
+    }
+
+    //* SLASH COMMANDS
+
+}
