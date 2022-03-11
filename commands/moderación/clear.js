dd06537f9e702664df9e3a8147ba7c8c53ee2b68
Maltazard <markus.yarleque@gmail.com>
2022-03-11 01:20:58 -0500

    mb

--- commands/moderación/clear.js
+++ commands/moderación/clear.js
@@ -62,7 +62,7 @@ module.exports =  {
     
         ]}).then(m => setTimeout(() => m.delete(), 5000)).catch((e) => console.log('Error al enviar mensaje: '+e))
         
-        let purge = parseInt(args[0] + 1)
+        let purge = args[0] + 1
 
         await message.channel.messages.fetch({ limit: purge }).then(
               
