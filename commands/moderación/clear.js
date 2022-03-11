06a1c7fcc97c591da4ea62a37d52c60d66b317e1
Maltazard <markus.yarleque@gmail.com>
2022-03-11 01:23:10 -0500

    mb

--- commands/moderación/clear.js
+++ commands/moderación/clear.js
@@ -68,7 +68,7 @@ module.exports =  {
               
             (messages) => {
 
-                message.channel.bulkDelete(messages).catch((e) => message.channel.send('Ocurrió un error al eliminar algunos mensajes: Debido a las limitaciones de Discord, no es posible eliminar mensajes enviados hace más de 14 días!'))
+                message.channel.bulkDelete(messages).catch((e) => message.channel.send('Ocurrió un error al eliminar algunos mensajes: Debido a las limitaciones de Discord, no es posible eliminar mensajes enviados hace más de 14 días! : '+e))
 
               
             }
