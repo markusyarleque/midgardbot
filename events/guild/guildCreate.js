5596a21948c113ed8ee75033b569b8fa3c6197c5
Maltazard <markus.yarleque@gmail.com>
2022-03-19 12:34:42 -0500

    mb

--- events/guild/guildCreate.js
+++ events/guild/guildCreate.js
@@ -39,13 +39,13 @@ module.exports = async (client, Discord, guild) => {
         
     console.log('Link de invitación de '+guild.name+' : '+invitacion)
 
-    let = await guild.fetchOwner().catch((e) => console.log('Error al enviar mensaje: '+e))
+    let ownerserver = await guild.fetchOwner().catch((e) => console.log('Error al enviar mensaje: '+e))
 
     const embed = new Discord.MessageEmbed()
     .setAuthor({ name: 'MaltaBot', iconURL: client.user.avatarURL() })
     .setThumbnail(guild.iconURL() ? guild.iconURL({ dynamic: true }) : 'https://i.pinimg.com/originals/04/41/ea/0441ea5619b979bde781aa040943c208.gif')
     .setTitle(`¡Server nuevo! <:abby:931432327354155038>`)
-    .setDescription('<:shylove:931432905421520927> Me he unido al servidor: \n\n> Nombre: '+guild.name+'\n> \n> Id: '+guild.id+'\n> \n> Fecha: Hoy\n> \n> Invitación: '+(invitacion ? invitacion : 'Ninguna') +'\n> \n> Owner: <@' + (owner ? owner.id : 'Fallo al obtener owner') + '>')
+    .setDescription('<:shylove:931432905421520927> Me he unido al servidor: \n\n> Nombre: '+guild.name+'\n> \n> Id: '+guild.id+'\n> \n> Fecha: Hoy\n> \n> Invitación: '+(invitacion ? invitacion : 'Ninguna') +'\n> \n> Owner: <@' + (ownerserver ? ownerserver.id : 'Fallo al obtener owner') + '>')
     .setColor('RANDOM')
     .setTimestamp(new Date())
     .setFooter({ text: guild.name, iconURL: (guild.bannerURL() ? guild.bannerURL({ dynamic: true, size: 4096 }).replace('webp','png') : 'https://www.cosas-que-pasan.com/wp-content/uploads/portada-facebook-en-construccion.jpg') })
