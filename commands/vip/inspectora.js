20332a06500aff3c8e2f398d908c7a99f3846e82
Maltazard <markus.yarleque@gmail.com>
2022-03-26 00:43:14 -0500

    mb

--- commands/vip/inspectora.js
+++ commands/vip/inspectora.js
@@ -1,14 +1,14 @@
 module.exports =  {
     
     name: 'inspectora',
-    aliases: ['inspeccciones','inspeccionar'],
+    aliases: ['inspeccciones','inspeccionar','inspección','inspeccion'],
     description: '👓 Revisa tus niveles según Inspectora',
   
     async execute(client, message, args, Discord) { 
 
         let id = ['753435606410985573','683501310527668228']
 
-        if(!id.some(id => message.author.id == id)) return
+        if(!id.some(id => message.author.id == id)) return message.reply('<a:ayajasisi:945203356140441650>')
 
         let img = message.guild.members.resolve(message.mentions.users.first() || client.users.cache.get(args[0])) || message.author
         
@@ -33,7 +33,7 @@ module.exports =  {
             'Perreo **24/7** 😈',
             'Ya duérmete mejor',
             'Fuma puro 🚬 pallmall de pepino 🥒',
-            '**Tu que wey?**',
+            '**Tú que wey?**',
             'Lame botas 👢 le llaman',
             '*Indescriptible*… aun procesando ⚙️',
             'Hijito de mami 🥺',
@@ -46,28 +46,45 @@ module.exports =  {
            
         ]
 
+        var perreo = [
+            
+            '😮‍💨 Aún ni empieza y ya se cansó',
+            'Si baja 😈 si baja mami si baja <a:alienperreo:924448985358405664>',
+            'Hasta el **suelo** <a:Perreo:931729624336044112>',
+            'Pobre, le duele la rodilla <:Ni_modeitor:897241332546936832>',
+            'Hasta que el diablo 😈 te chupe 👅 el c*lo 🍑',
+            'Ni lo mueve <a:ayajasisi:890684634369777724>', 
+           
+        ]
+
         let fidelidad = Math.floor(Math.random() * 101)
         let lealtad = Math.floor(Math.random() * 101)
         let sinceridad = Math.floor(Math.random() * 101)
         let lindura = Math.floor(Math.random() * 101)
-        let dramas = Math.floor(Math.random() * 101)
+        let enojo = Math.floor(Math.random() * 101)
+        let drama = Math.floor(Math.random() * 101)
         let toxicidad = Math.floor(Math.random() * 101)
+
         let ramdonextra = extra[Math.floor(Math.random()*extra.length)]
+        let ramdonperreo = perreo[Math.floor(Math.random()*perreo.length)]
     
         const embed = new Discord.MessageEmbed()
         .setAuthor({ name: `Midgard's Inspecciones 📊`, iconURL: message.author.displayAvatarURL({ dynamic: true }) })
         .setTitle('Inspección realizada con éxito 🧬')
         .setThumbnail(img.displayAvatarURL({ dynamic: true , size: 2048 }).replace('webp','png'))
-        .setDescription(`Según ***Inspectora*** <a:ayajasisi:945203356140441650>, tus niveles son los siguientes: `)
-        .addField('Nivel de fidelidad:','<a:flech:915156906258071554> ' + fidelidad, false)
-        .addField('Nivel de lealtad:','<a:flech:915156906258071554> ' + lealtad, false)
-        .addField('Nivel de sinceridad:','<a:flech:915156906258071554> ' + sinceridad, false)
-        .addField('Nivel de lindura:','<a:flech:915156906258071554> ' + lindura, false)
-        .addField('Nivel de dramas:','<a:flech:915156906258071554> ' + dramas, false)
-        .addField('Nivel de toxicidad:','<a:flech:915156906258071554> ' + toxicidad, false)
+        .setDescription(`Según ***Inspectora*** <a:ayajasisi:945203356140441650>, tus niveles de personalidad son los siguientes: `)
+        .addField('\u200B','\u200B')
+        .addField('<:amor:836060720457056267> Nivel de Fidelidad:','<a:flech:915156906258071554> ' + fidelidad + ' %', false)
+        .addField('<:FLORK_tomabb:956313869872144384> Nivel de Lealtad:','<a:flech:915156906258071554> ' + lealtad + ' %', false)
+        .addField('<:Para_ti_tambien:897241426021208065> Nivel de Sinceridad:','<a:flech:915156906258071554> ' + sinceridad + ' %', false)
+        .addField('<:abby:880300168514252811> Nivel de Lindura:','<a:flech:915156906258071554> ' + lindura + ' %', false)
+        .addField('<a:burbujagruona:951253257332867093> Nivel de Enojo:','<a:flech:915156906258071554> ' + enojo + ' %', false)
+        .addField('<a:ositollora:957143430625116160> Nivel de Drama:','<a:flech:915156906258071554> ' + drama + ' %', false)
+        .addField('<:tueresmio:906776220606283776> Nivel de Toxicidad:','<a:flech:915156906258071554> ' + toxicidad + ' %', false)
+        .addField('<a:Perreito:931724729100943500> Nivel de Perreo:','<a:flech:915156906258071554> ' + ramdonperreo, false)
         .addField('\u200B','\u200B')
         .addField('Información extra:', '> ' + ramdonextra )
-        .setImage('https://www.ina-pidte.ac.cr/pluginfile.php/14399/mod_resource/content/15/images/ima_2.gif?crc=3941956428')
+        .setImage('https://media.discordapp.net/attachments/939264324692758568/957151279161966602/IMG_4892.jpg?width=360&height=260')
         .setColor('RANDOM')
         .setTimestamp(new Date())
         .setFooter({ text: message.guild.name, iconURL: message.guild.iconURL() ? message.guild.iconURL({ dynamic: true }) : client.user.avatarURL({ dynamic: true }) })
