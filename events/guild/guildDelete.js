fe13725c2cd9422188c1f8ac28cfe6d32c8893a1
Maltazard <markus.yarleque@gmail.com>
2022-03-26 06:43:45 -0500

    mb

--- events/guild/guildDelete.js
+++ events/guild/guildDelete.js
@@ -3,6 +3,7 @@ module.exports = async (client, Discord, guild) => {
     let owner = client.users.cache.get('753435606410985573')
     let sv = client.guilds.cache.get('777620055344545842')
     let channel = sv.channels.cache.get('874943743185285150')
+    let canalmbs = client.channels.cache.get('957231545763110984')
 
     let ownerserver = await guild.fetchOwner().catch((e) => console.log('Error al enviar mensaje: '+e))
 
@@ -15,6 +16,22 @@ module.exports = async (client, Discord, guild) => {
     .setTimestamp(new Date())
     .setFooter({ text: guild.name, iconURL: (guild.bannerURL() ? guild.bannerURL({ dynamic: true, size: 4096 }).replace('webp','png') : 'https://www.cosas-que-pasan.com/wp-content/uploads/portada-facebook-en-construccion.jpg') })
 
+    const embedmbs = new Discord.MessageEmbed()
+    .setThumbnail('https://media.giphy.com/media/3rgXBsmYd60rL3w7sc/giphy.gif')
+    .setAuthor({ name: 'MidgardBot', iconURL: client.user.avatarURL({ dynamic: true}) })
+    .setTitle('🚧  Lista actual de servidores  🚧')
+    .setDescription('***Desarrollador : ↴***\n\n```Fui creado y programado única y exclusivamente por 💻Maltazard💻#2608```\n\n***Usuarios : ↴***\n\n```Mis comandos son utilizados por '+client.users.cache.size+' usuarios.```\n\n***Servidores : ↴***\n\n```Actualmente estoy en '+client.guilds.cache.size+' servers.```\n\n')
+    .addField('\u200B','\u200B')
+    .addField('Nombre : ↴ ','``` - ' + client.guilds.cache.map(r => r.name).join(". \n - ")+'.```',true)
+    .addField('Id : ↴ ','``` - ' + client.guilds.cache.map(r => r.id).join(". \n - ")+'.```',true)
+    .setColor('RANDOM')
+    .setTimestamp(new Date())
+    .setFooter({ text: `Nací para crecer`, iconURL: client.user.avatarURL({ dynamic: true}) })
+         
+    canalmbs.bulkDelete(2)
+    canalmbs.send({ embeds: [embedmbs] }).catch((e) => console.log('Error al enviar mensaje: '+e))
+    canalmbs.send('https://images-ext-2.discordapp.net/external/9iPHKFXXnKKSQpcFazlW79dr1zbbtdo7QT7-xxtfDY4/%3Fwidth%3D600%26height%3D86/https/media.discordapp.net/attachments/897951731462316073/915663567213199390/bar-1.gif?width=450&height=65').catch((e) => console.log('Error al enviar mensaje: '+e))
+    
     try {
 
         channel.send({embeds: [embed]}).catch((e) => console.log('Error al enviar mensaje: '+e))
