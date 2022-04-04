b93e9d6e5cbe0c559bf4bb85d0b55c8827038c21
Maltazard <markus.yarleque@gmail.com>
2022-04-04 05:45:50 -0500

    mb

--- commands/información/setprefix.js
+++ commands/información/setprefix.js
@@ -115,7 +115,7 @@ module.exports = {
                 .setDescription('<a:Verify1:931463354357276742> | **Prefix cambiado con éxito:** ')
                 .addField('\u200B','<a:barra:889717671044726824><a:barra:889717671044726824><a:barra:889717671044726824><a:barra:889717671044726824><a:barra:889717671044726824><a:barra:889717671044726824><a:barra:889717671044726824><a:barra:889717671044726824><a:barra:889717671044726824><a:barra:889717671044726824>')
                 .addField('Servidor: ', '<a:flech:931432469935312937> `' + message.guild.name + '`')
-                .addField('Nuevo Prefix: ', '<a:flech:931432469935312937> `' + newprefix + '`\n\n<a:barra:889717671044726824><a:barra:889717671044726824><a:barra:889717671044726824><a:barra:889717671044726824><a:barra:889717671044726824><a:barra:889717671044726824><a:barra:889717671044726824><a:barra:889717671044726824><a:barra:889717671044726824><a:barra:889717671044726824>')
+                .addField('Nuevo Prefix: ', '<a:flech:931432469935312937> `' + newprefix + '`\n<a:barra:889717671044726824><a:barra:889717671044726824><a:barra:889717671044726824><a:barra:889717671044726824><a:barra:889717671044726824><a:barra:889717671044726824><a:barra:889717671044726824><a:barra:889717671044726824><a:barra:889717671044726824><a:barra:889717671044726824>')
                 .setTimestamp()
 
                 setTimeout(() => {
