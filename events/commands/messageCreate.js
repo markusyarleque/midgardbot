4e1c4de18e181a8f751601b18cfe07f7488d7c64
Maltazard <markus.yarleque@gmail.com>
2022-05-02 20:01:09 -0500

    masass

--- events/commands/messageCreate.js
+++ events/commands/messageCreate.js
@@ -1111,7 +1111,7 @@ module.exports = async (client, Discord, message) => {
                 dem = JSON.stringify(m.embeds, null, 2)
                 // dem = m.embeds
 
-                // fields = Object.values(dem)
+                fields = dem[0].title
 
                 // idxpclub = Object.values(fields)
                 
