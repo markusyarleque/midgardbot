a305ed96d88603ea13d9eeb2796910cbd71182c5
Maltazard <markus.yarleque@gmail.com>
2022-05-03 06:19:08 -0500

    hbas

--- events/commands/messageCreate.js
+++ events/commands/messageCreate.js
@@ -1135,7 +1135,7 @@ module.exports = async (client, Discord, message) => {
 
                 fields = JSON.stringify(m.embeds, ['fields','value'], 2)
 
-                idxpclub = fields.fields.value
+                idxpclub = ' ' + fields.fields.value
                 
                 // xpclub = Object.values(idxpclub)
 
